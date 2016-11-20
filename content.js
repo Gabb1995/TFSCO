2a30390d6bfea54ae8dbc17602f627393ec0f67f
Kopatch@gmail.com <Sublime123>
2016-11-21 01:57:17 +0200

    fixed bugs added opacity input

--- content.js
+++ content.js
@@ -2,7 +2,9 @@ $( document ).ready(function() {
 
   var body =document.querySelector('body');
   var chatBox;
-  
+  var savedStyle;
+  var chatOpacity;
+
   var minimize = false;
   var checkExist = setInterval(function() {
      if ($('#right_col').length) {
@@ -20,6 +22,7 @@ $( document ).ready(function() {
     if (!minimize){
         addChatMinimize();
     }
+    addChatSettings();
     //make sure click only happens once.
     var counter = 0;
     if(counter === 0){
@@ -42,13 +45,39 @@ $( document ).ready(function() {
     chatBox.draggable('disable');
   }
 
+
+  function rangeOnChange(){
+    $(document).on('input change', '#CS_opacity', function() {
+       chatBox[0].style.opacity = this.value / 100;
+    });
+  }
+
+  function addChatSettings(){
+     var chatSettings = document.createElement('div');
+     chatSettings.classList.add('TFCO_chatSettings', 'TFCO_chatButton');
+     chatBox[0].appendChild(chatSettings);
+     chatSettings.addEventListener('click', function(){
+         body.classList.toggle('TFCO_settingsOpen');
+      });
+
+
+      var chatSettingsBoxContainer = document.createElement('div');
+      var chatSettingsBox = "<div class='CS_box'>"+
+                              "<label for='CS_opacity'></label>"+
+                              "<input id='CS_opacity' type='range' min='10' value='50' max='100'></input>"+
+                            "</div>";
+       chatBox[0].appendChild(chatSettingsBoxContainer);
+       chatSettingsBoxContainer.innerHTML = chatSettingsBox;
+  }
+
+
   function addChatMinimize(){
 
     minimize = true;
 
     //create chat minimize button
     var chatMinimze = document.createElement('div');
-    chatMinimze.classList.add('TFCO_chatMinimize');
+    chatMinimze.classList.add('TFCO_chatMinimize', 'TFCO_chatButton');
     chatBox[0].appendChild(chatMinimze);
 
     chatMinimze.addEventListener('click', function(){
@@ -61,32 +90,35 @@ $( document ).ready(function() {
 
   //when entering fullscreen and out
   function changeHandler(){
-    var savedStyle;
+   
 
     clearTimeout(handlerTimeout);
     handlerTimeout = setTimeout(function(){
+        //ENTER FULLSCREEN
         if( window.innerHeight === screen.height) {
           console.log('go fullscreen');
           chrome.runtime.sendMessage({action: "enterFullscreen"});
           chatBox.draggable('enable');
           chatBox.resizable();
+          rangeOnChange();
           if(savedStyle){
               chatBox[0].setAttribute('style', savedStyle);
+              document.getElementById('#CS_opacity').value = chatOpacity;
           }
-      
+        //EXIT FULLSCREEN
         } else {
-          console.log('exit fullscreen');
-
           chrome.runtime.sendMessage({action: "exitFullscreen"});
           body.classList.remove('TFCO_minimized');
           if (body.classList.contains('TFCO_fullScreenMode')){
             savedStyle = chatBox[0].style.cssText;
+            chatOpacity = chatBox[0].style.opacity;
           }
           body.classList.remove('TFCO_fullScreenMode');
           chatBox.removeAttr("style");
           chatBox.draggable('disable');
+          console.log('exit fullscreen');
         }
-    },500);
+    },100);
   }
 
   if (document.addEventListener){
@@ -99,14 +131,7 @@ $( document ).ready(function() {
   function appendDom(){
     var fsToolBar;
     var fsButton;
-    fsToolBar = "<div class='fsToolBar'>"+
-                  "<div class='fsToolBar-title>Fullscreen with chat</div>"+
-                  "<div class='fsToolBar-chatOpacity'>"+
-                    "<label>Chat opacity</label>"+
-                    "<input type='number' />"+
-                  "</div>"+
-                  "<div class='fullscreenBtn'>Go!</div>"+
-                "</div>"+
+    fsToolBar = "<div class='fullscreenBtn'>Go!</div>"+
                 "<div class='loading'>Waiting for chat to load...</div>";
 
     fsContainer.innerHTML = fsToolBar;
